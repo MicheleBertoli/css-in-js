cfc5af1eabd6245d8b1c8370ad9fa60fcfb9a487
martinandert <mandert@gmail.com>
2015-05-29 16:42:14 +0200

    make styles consistent with other examples

--- react-inline/button.js
+++ react-inline/button.js
@@ -4,7 +4,7 @@ import StyleSheet from 'react-inline';
 class Button extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className={styles.container}>
         <button className={styles.button}>Click me!</button>
       </div>
     );
@@ -12,62 +12,29 @@ class Button extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    textAlign: 'center'
+  },
+
   button: {
-    padding: '6px 12px',
-    fontSize: 14,
-    lineHeight: 1.5,
-    touchAction: 'manipulation',
-    cursor: 'pointer',
-    userSelect: 'none',
-    border: '1px solid transparent',
-    borderRadius: 4,
-    color: '#fff',
-    backgroundColor: '#337ab7',
-    borderColor: '#2e6da4',
-
-    '@media only screen and (max-width: 640px)': {
-      display: 'block',
-      width: '100%'
-    },
-
-    ':focus': {
-      color: '#fff',
-      backgroundColor: '#286090',
-      borderColor: '#122b40',
-      outline: 'thin dotted',
-      outlineOffset: -2
-    },
+    backgroundColor: '#ff0000',
+    width: 320,
+    padding: 20,
+    borderRadius: 5,
+    border: 'none',
+    outline: 'none',
 
     ':hover': {
-      color: '#fff',
-      backgroundColor: '#286090',
-      borderColor: '#204d74',
-      textDecoration: 'none'
+      color: '#fff'
     },
 
     ':active': {
-      color: '#fff',
-      backgroundColor: '#286090',
-      borderColor: '#204d74',
-      backgroundImage: 'none',
-      outline: 0,
-      boxShadow: 'inset 0 3px 5px rgba(0, 0, 0, .125)',
-
-      ':hover': {
-        color: '#fff',
-        backgroundColor: '#204d74',
-        borderColor: '#122b40'
-      }
+      position: 'relative',
+      top: 2
     },
 
-    '[disabled]': {
-      backgroundColor: '#337ab7',
-      borderColor: '#2e6da4',
-      cursor: 'not-allowed',
-      filter: 'alpha(opacity=65)',
-      boxShadow: 'none',
-      opacity: .65,
-      pointerEvents: 'none'
+    '@media (max-width: 480px)': {
+      width: 160
     }
   }
 });
