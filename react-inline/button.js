b5a68cb0317fdfb3dc7579ed2892f6ea69ce849e
Michele Bertoli <michele@berto.li>
2015-06-28 08:50:43 +0100

    updated react-inline

--- react-inline/button.js
+++ react-inline/button.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import StyleSheet from 'react-inline';
 
+let styles = StyleSheet.create({
+  container: {
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#ff0000',
+    width: 320,
+    padding: 20,
+    borderRadius: 5,
+    border: 'none',
+    outline: 'none',
+    ':hover': {
+      color: '#fff'
+    },
+    ':active': {
+      position: 'relative',
+      top: 2
+    },
+    '@media (max-width: 480px)': {
+      width: 160
+    }
+  }
+});
+
 class Button extends React.Component {
   render() {
     return (
@@ -10,33 +34,5 @@ class Button extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    textAlign: 'center'
-  },
-
-  button: {
-    backgroundColor: '#ff0000',
-    width: 320,
-    padding: 20,
-    borderRadius: 5,
-    border: 'none',
-    outline: 'none',
-
-    ':hover': {
-      color: '#fff'
-    },
-
-    ':active': {
-      position: 'relative',
-      top: 2
-    },
-
-    '@media (max-width: 480px)': {
-      width: 160
-    }
-  }
-});
 
 React.render(<Button />, document.getElementById('content'));
