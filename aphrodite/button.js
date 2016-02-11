2b6d06670cd2c10f2d40635a568ba2fa60f6bc12
Michele Bertoli <michele@berto.li>
2016-02-11 20:12:24 +0000

    aphrodite

---
+++ aphrodite/button.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {StyleSheet, css} from 'aphrodite';
+
+const styles = StyleSheet.create({
+  container: {
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#ff0000',
+    width: '320px',
+    padding: '20px',
+    borderRadius: '5px',
+    border: 'none',
+    outline: 'none',
+    ':hover': {
+      color: '#fff',
+    },
+    ':active': {
+      position: 'relative',
+      top: '2px'
+    },
+    '@media (max-width: 480px)': {
+      width: '160px'
+    }
+  }
+});
+
+class Button extends Component {
+  render() {
+    return (
+      <div className={css(styles.container)}>
+        <button className={css(styles.button)}>Click me!</button>
+      </div>
+    );
+  }
+}
+
+React.render(<Button />, document.getElementById('content'));
