c0a62c46b90a57477b6e39a407304b0593978b49
rofrischmann <robin@rofrischmann.de>
2016-07-07 19:06:01 +0200

    remove projects from ghpages branch

--- radium/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel?stage=0'
-    }]
-  }
-}
--- reactcss/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loaders: ['babel', 'react-map-styles']
-    }]
-  }
-}
