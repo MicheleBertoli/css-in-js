31d6d211343bee47b07d39c227c9f39acc6dabd9
Michele Bertoli <michele@berto.li>
2016-12-12 08:18:57 +0000

    css-constructor

---
+++ css-constructor/button.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import css from 'css-constructor';
+
+class Container extends Component {
+
+  @css`
+    text-align: center;
+  `;
+
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+
+}
+
+class Button extends Component {
+
+  @css`
+    background-color: #ff0000;
+    width: 320px;
+    padding: 20px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+  `;
+
+  render() {
+    return <button>{this.props.children}</button>;
+  }
+
+}
+
+const App = () => (
+  <Container>
+    <Button>Click me!</Button>
+  </Container>
+);
+
+render(<App />, document.getElementById('content'));
---
+++ css-constructor/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react'],
+        plugins: ['transform-decorators-legacy']
+      }
+    }]
+  }
+}
