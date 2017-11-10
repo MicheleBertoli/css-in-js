7e28896beee4a773dfd2e67da1d7ca480d78e808
Vadim Dalecky <streamich@gmail.com>
2017-11-10 17:35:22 +0000

    style: PR review

--- freestyler/webpack.config.js
+++ freestyler/webpack.config.js
@@ -2,16 +2,16 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react'],
-                    plugins: ["transform-decorators-legacy"]
-                }
-            }
-        ]
-    }
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ["transform-decorators-legacy"]
+        }
+      }
+    ]
+  }
 };
