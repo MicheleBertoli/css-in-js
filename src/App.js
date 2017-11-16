92ec2f1db224a57948e27da202e2a1551b561e9f
stereobooster <stereobooster@gmail.com>
2017-11-16 19:18:13 +0100

    Allign with current data from readme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Table from "./components/Table";
+import { default as data } from "./data.json";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Table />
+        <Table data={data} />
       </div>
     );
   }
